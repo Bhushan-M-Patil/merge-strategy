b757a890d37a434e42275a2d39671a2f58aa5c73
Bhushan-M-Patil <bhushanpatil3353@gmail.com>
2023-05-15 18:17:12 +0530

    2nd main commit

--- java1.java
+++ java1.java
@@ -10,7 +10,6 @@ n1 =n2;
 n2 = temp;
 System.out.println("After swapping no. are :"+n1+"  "+n2);
 System.out.println("Done in development phase");
-System.out.println("Tested !");
 sc.close();
 }
 }
