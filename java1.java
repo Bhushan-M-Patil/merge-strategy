592f49c5be4d063a540fd0c04a703cfaf4217660
Bhushan-M-Patil <bhushanpatil3353@gmail.com>
2023-05-15 18:09:58 +0530

    development commit

---
+++ java1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class java1{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+int n1= sc.nextInt();
+int n2= sc.nextInt();
+System.out.println("Before swapping no. are :"+n1+"  "+n2);
+int temp = n1;
+n1 =n2;
+n2 = temp;
+System.out.println("After swapping no. are :"+n1+"  "+n2);
+System.out.println("Done in development phase");
+sc.close();
+}
+}
